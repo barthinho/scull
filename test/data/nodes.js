784fa70463e9ba7953036ccddfd60d38f9adc342
Thomas Urban <thomas.urban@cepharum.de>
2018-03-24 23:31:49 +0100

    revising manager for pool of valid nodes' addresses to handle address of local node specially

--- test/data/nodes.js
+++ test/data/nodes.js
@@ -51,7 +51,13 @@ suite( "Manager for pool of nodes", () => {
 		new Nodes( ["/ip4/127.0.0.1/tcp/12345"] ).addresses.should.be.Array().which.has.length( 1 );
 	} );
 
-	test( "normalized provided addresses provided on creation", () => {
+	test( "exposes copy of list of currently valid nodes' addresses", () => {
+		const pool = new Nodes();
+
+		pool.addresses.should.be.Array().which.is.empty().and.not.equal( pool.addresses );
+	} );
+
+	test( "normalizes provided addresses provided on creation", () => {
 		const pool = new Nodes( [
 			"/ip4/127.0.0.1/tcp/1",
 			{ id: "/ip4/127.0.0.1/tcp/2" },
@@ -67,7 +73,70 @@ suite( "Manager for pool of nodes", () => {
 		pool.addresses[4].should.be.instanceOf( Address.Address ).which.has.property( "id" ).which.is.equal( "/ip4/127.0.0.1/tcp/5" );
 	} );
 
-	test( "permits addition of another node after creation", () => {
+	test( "can be instantiated w/ additionally provided local address", () => {
+		( () => new Nodes( [], "/ip4/127.0.0.1/tcp/1" ) ).should.not.throw();
+		( () => new Nodes( [], { id: "/ip4/127.0.0.1/tcp/2" } ) ).should.not.throw();
+		( () => new Nodes( [], { address: "/ip4/127.0.0.1/tcp/3" } ) ).should.not.throw();
+		( () => new Nodes( [], { address: "127.0.0.1", port: 4 } ) ).should.not.throw();
+		( () => new Nodes( [], Address( "/ip4/127.0.0.1/tcp/5" ) ) ).should.not.throw();
+	} );
+
+	test( "does not expose provided address of local node w/ list of currently valid nodes' addresses", () => {
+		const addresses = [
+			"/ip4/127.0.0.1/tcp/1",
+			{ id: "/ip4/127.0.0.1/tcp/2" },
+			{ address: "/ip4/127.0.0.1/tcp/3" },
+			{ address: "127.0.0.1", port: 4 },
+			Address( "/ip4/127.0.0.1/tcp/5" ),
+		];
+
+		const withoutLocal = new Nodes( addresses );
+		withoutLocal.addresses.should.have.length( 5 );
+		Boolean( withoutLocal.addresses.find( a => a.matches( "/ip4/127.0.0.1/tcp/3" ) ) ).should.be.true();
+
+		const withLocal = new Nodes( addresses, "/ip4/127.0.0.1/tcp/3" );
+		withLocal.addresses.should.have.length( 4 );
+		Boolean( withLocal.addresses.find( a => a.matches( "/ip4/127.0.0.1/tcp/3" ) ) ).should.be.false();
+	} );
+
+	test( "exposes method `has()` for testing addresses whether selecting nodes of cluster or not", () => {
+		const pool = new Nodes( ["/ip4/127.0.0.1/tcp/1"] );
+		pool.add( { id: "/ip4/127.0.0.1/tcp/2" } );
+
+		pool.has( "/ip4/127.0.0.1/tcp/1" ).should.be.true();
+		pool.has( { id: "/ip4/127.0.0.1/tcp/2" } ).should.be.true();
+		pool.has( { address: "/ip4/127.0.0.1/tcp/1" } ).should.be.true();
+		pool.has( { address: "127.0.0.1", port: 2 } ).should.be.true();
+		pool.has( Address( "/ip4/127.0.0.1/tcp/1" ) ).should.be.true();
+
+		pool.has( "/ip4/127.0.0.1/tcp/3" ).should.be.false();
+		pool.has( { id: "/ip4/127.0.0.1/tcp/4" } ).should.be.false();
+		pool.has( { address: "/ip4/127.0.0.1/tcp/3" } ).should.be.false();
+		pool.has( { address: "127.0.0.1", port: 4 } ).should.be.false();
+		pool.has( Address( "/ip4/127.0.0.1/tcp/3" ) ).should.be.false();
+	} );
+
+	test( "considers falsy addresses to be never valid", () => {
+		const pool = new Nodes( ["/ip4/127.0.0.1/tcp/1"] );
+
+		pool.has().should.be.false();
+		pool.has( false ).should.be.false();
+		pool.has( undefined ).should.be.false();
+		pool.has( null ).should.be.false();
+		pool.has( "" ).should.be.false();
+	} );
+
+	test( "considers optionally provided local address to be always part of cluster", () => {
+		const pool = new Nodes( [], "/ip4/127.0.0.1/tcp/1" );
+
+		pool.has( "/ip4/127.0.0.1/tcp/1" ).should.be.true();
+		pool.has( { id: "/ip4/127.0.0.1/tcp/1" } ).should.be.true();
+		pool.has( { address: "/ip4/127.0.0.1/tcp/1" } ).should.be.true();
+		pool.has( { address: "127.0.0.1", port: 1 } ).should.be.true();
+		pool.has( Address( "/ip4/127.0.0.1/tcp/1" ) ).should.be.true();
+	} );
+
+	test( "permits addition of another address after creation", () => {
 		( () => new Nodes().add( "/ip4/127.0.0.1/tcp/1" ) ).should.not.throw();
 		( () => new Nodes().add( { id: "/ip4/127.0.0.1/tcp/2" } ) ).should.not.throw();
 		( () => new Nodes().add( { address: "/ip4/127.0.0.1/tcp/3" } ) ).should.not.throw();
@@ -75,7 +144,7 @@ suite( "Manager for pool of nodes", () => {
 		( () => new Nodes().add( Address( "/ip4/127.0.0.1/tcp/5" ) ) ).should.not.throw();
 	} );
 
-	test( "notifies on addition of another node w/ normalized address", () => {
+	test( "notifies on addition of another address w/ normalized address", () => {
 		return new Promise( resolve => {
 			const pool = new Nodes();
 
@@ -88,7 +157,7 @@ suite( "Manager for pool of nodes", () => {
 			} );
 	} );
 
-	test( "omits notification on adding node already existing in pool", () => {
+	test( "omits notification on adding address already existing in pool", () => {
 		return new Promise( ( resolve, reject ) => {
 			const pool = new Nodes( ["/ip4/127.0.0.1/tcp/1"] );
 
@@ -98,11 +167,29 @@ suite( "Manager for pool of nodes", () => {
 
 			pool.add( "/ip4/127.0.0.1/tcp/1" );
 
-			setTimeout( resolve, 500 );
+			setTimeout( resolve, 100 );
 		} );
 	} );
 
-	test( "permits removal of another node after creation", () => {
+	test( "omits notification on adding address of local node", () => {
+		return new Promise( ( resolve, reject ) => {
+			const pool = new Nodes( [], "/ip4/127.0.0.1/tcp/1" );
+
+			pool.once( "added", () => {
+				reject( new Error( "unexpected notification" ) );
+			} );
+
+			pool.add( "/ip4/127.0.0.1/tcp/1" );
+
+			setTimeout( () => {
+				pool.addresses.should.be.empty();
+
+				resolve();
+			}, 100 );
+		} );
+	} );
+
+	test( "permits removal of another address after creation", () => {
 		( () => new Nodes().remove( "/ip4/127.0.0.1/tcp/1" ) ).should.not.throw();
 		( () => new Nodes().remove( { id: "/ip4/127.0.0.1/tcp/2" } ) ).should.not.throw();
 		( () => new Nodes().remove( { address: "/ip4/127.0.0.1/tcp/3" } ) ).should.not.throw();
@@ -110,7 +197,7 @@ suite( "Manager for pool of nodes", () => {
 		( () => new Nodes().remove( Address( "/ip4/127.0.0.1/tcp/5" ) ) ).should.not.throw();
 	} );
 
-	test( "notifies on addition of another node w/ normalized address", () => {
+	test( "notifies on removal of another address w/ normalized address", () => {
 		return new Promise( resolve => {
 			const pool = new Nodes( ["/ip4/127.0.0.1/tcp/1"] );
 
@@ -123,7 +210,7 @@ suite( "Manager for pool of nodes", () => {
 			} );
 	} );
 
-	test( "omits notification on adding node already existing in pool", () => {
+	test( "omits notification on removing address missing in pool", () => {
 		return new Promise( ( resolve, reject ) => {
 			const pool = new Nodes();
 
@@ -133,7 +220,24 @@ suite( "Manager for pool of nodes", () => {
 
 			pool.remove( "/ip4/127.0.0.1/tcp/1" );
 
-			setTimeout( resolve, 500 );
+			setTimeout( resolve, 100 );
+		} );
+	} );
+
+	test( "omits notification on removing local address", () => {
+		return new Promise( ( resolve, reject ) => {
+			const pool = new Nodes( ["/ip4/127.0.0.1/tcp/1"], "/ip4/127.0.0.1/tcp/1" );
+
+			pool.once( "removed", () => {
+				reject( new Error( "unexpected notification" ) );
+			} );
+
+			pool.remove( "/ip4/127.0.0.1/tcp/1" );
+
+			setTimeout( () => {
+				pool.addresses.should.have.length( 1 );
+				resolve();
+			}, 100 );
 		} );
 	} );
 } );
