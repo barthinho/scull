40dc078504a7de3d1fd3f367b0332a42c225e389
Thomas Urban <thomas.urban@cepharum.de>
2019-10-15 17:58:02 +0200

    adding separate suite of heavier resilience tests

---
+++ test/resilience/resilience-chaos-large-disk.plus.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const { suite, test, setup, teardown } = require( "mocha" );
+
+const Setup = require( "./context/setup" );
+const ResilienceTestClient = require( "./context/client" );
+
+
+// fails due to exceeding maximum number of listeners for event emitters
+suite.skip( "resilience, large cluster, chaos, on disk", function() {
+	this.timeout( 30000 );
+
+	const { before, after, addresses } = Setup( {
+		chaos: true,
+		persist: true,
+		nodeCount: 7,
+	} );
+
+	setup( before );
+	teardown( after );
+
+
+	test( "works", function() {
+		this.timeout( 125000 );
+
+		return new Promise( ( resolve, reject ) => {
+			let timeout = null;
+
+
+			const client = new ResilienceTestClient( addresses, {
+				duration: 120000,
+			} );
+
+			resetOperationTimeout();
+			client.on( "operation", resetOperationTimeout );
+			client.on( "warning", () => {
+				reject( new Error( "server process writing to stderr unexpectedly" ) );
+			} );
+
+			client.start()
+				.then( () => {
+					clearTimeout( timeout );
+					console.log( "stats: %j", client.stats ); // eslint-disable-line no-console
+					resolve();
+				} )
+				.catch( error => {
+					clearTimeout( timeout );
+					console.log( "stats: %j", client.stats ); // eslint-disable-line no-console
+					reject( error );
+				} );
+
+
+			/**
+			 * Handles single request in a sequence of testing requests having
+			 * timed out.
+			 *
+			 * @returns {void}
+			 */
+			function onOperationTimeout() {
+				reject( new Error( "no operation for more than 11 seconds" ) );
+			}
+
+			/**
+			 * Resets per-request timeout detection due to client having started
+			 * another request.
+			 *
+			 * @returns {void}
+			 */
+			function resetOperationTimeout() {
+				if ( timeout ) {
+					clearTimeout( timeout );
+				}
+
+				timeout = setTimeout( onOperationTimeout, 11000 );
+			}
+		} );
+	} );
+} );
---
+++ test/resilience/resilience-chaos-long-term-disk.plus.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const { suite, test, setup, teardown } = require( "mocha" );
+
+const Setup = require( "./context/setup" );
+const ResilienceTestClient = require( "./context/client" );
+
+
+suite( "resilience, chaos, on disk, longer term", function() {
+	this.timeout( 30000 );
+
+	const { before, after, addresses } = Setup( {
+		chaos: true,
+		persist: true,
+	} );
+
+	setup( before );
+	teardown( after );
+
+
+	test( "works", function() {
+		this.timeout( 3605000 );
+
+		return new Promise( ( resolve, reject ) => {
+			let timeout = null;
+
+
+			const client = new ResilienceTestClient( addresses, {
+				duration: 3600000,
+			} );
+
+			resetOperationTimeout();
+			client.on( "operation", resetOperationTimeout );
+			client.on( "warning", () => {
+				reject( new Error( "server process writing to stderr unexpectedly" ) );
+			} );
+
+			client.start()
+				.then( () => {
+					clearTimeout( timeout );
+					console.log( "stats: %j", client.stats ); // eslint-disable-line no-console
+					resolve();
+				} )
+				.catch( error => {
+					clearTimeout( timeout );
+					console.log( "stats: %j", client.stats ); // eslint-disable-line no-console
+					reject( error );
+				} );
+
+
+			/**
+			 * Handles single request in a sequence of testing requests having
+			 * timed out.
+			 *
+			 * @returns {void}
+			 */
+			function onOperationTimeout() {
+				reject( new Error( "no operation for more than 11 seconds" ) );
+			}
+
+			/**
+			 * Resets per-request timeout detection due to client having started
+			 * another request.
+			 *
+			 * @returns {void}
+			 */
+			function resetOperationTimeout() {
+				if ( timeout ) {
+					clearTimeout( timeout );
+				}
+
+				timeout = setTimeout( onOperationTimeout, 11000 );
+			}
+		} );
+	} );
+} );
