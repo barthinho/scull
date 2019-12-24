1c96b5a3d57515235b41e421b5fbbfac42c48fd0
Thomas Urban <thomas.urban@cepharum.de>
2019-12-25 00:40:52 +0100

    revising log service for silently capturing debug logs of all running nodes

--- test/resilience/context/log-server.js
+++ test/resilience/context/log-server.js
@@ -27,6 +27,7 @@
  */
 
 const UDP = require( "dgram" );
+const Util = require( "util" );
 
 
 /**
@@ -68,8 +69,9 @@ class LogServer {
 	 * @param {int} bufferSize maximum number of records to keep
 	 */
 	constructor( bufferSize = null ) {
-		const _size = Math.max( parseInt( bufferSize ) || process.env.DEBUG_LOG_BUFFER || 100, 100 );
+		const _size = Math.max( parseInt( bufferSize ) || process.env.DEBUG_LOG_BUFFER || 1000, 1000 );
 		let write = 0;
+		let captured = 0;
 
 		Object.defineProperties( this, {
 			/**
@@ -80,6 +82,47 @@ class LogServer {
 			 * @readonly
 			 */
 			logs: { value: new Array( _size ) },
+
+			/**
+			 * Exposes current number of entries in LogServer#logs.
+			 *
+			 * @name LogServer#write
+			 * @property {int}
+			 * @readonly
+			 */
+			write: { get: () => write },
+
+			/**
+			 * Exposes number of all captured log messages.
+			 *
+			 * @name LogServer#captured
+			 * @property {int}
+			 * @readonly
+			 */
+			captured: { get: () => captured },
+
+			/**
+			 * Collects provided message on behalf of server.
+			 *
+			 * @name LogServer#log
+			 * @property {function(format: string, ...args):void}
+			 * @readonly
+			 */
+			log: {
+				value: ( msg, rinfo ) => {
+					captured++;
+
+					if ( write === _size ) {
+						this.logs.splice( 0, 1 );
+						write--;
+					}
+
+					this.logs[write++] = {
+						peer: `${rinfo.address}:${rinfo.port}`,
+						msg,
+					};
+				}
+			},
 		} );
 
 		Object.defineProperties( this, {
@@ -92,17 +135,7 @@ class LogServer {
 			 * @protected
 			 */
 			_socket: {
-				value: UDP.createSocket( "udp4", ( msg, rinfo ) => {
-					if ( write === _size ) {
-						this.logs.splice( 0, 1 );
-						write--;
-					}
-
-					this.logs[write++] = {
-						peer: `${rinfo.address}:${rinfo.port}`,
-						msg
-					};
-				} ),
+				value: UDP.createSocket( "udp4", this.log ),
 			},
 		} );
 
@@ -156,15 +189,119 @@ class LogServer {
 	}
 
 	/**
+	 * Displays and collects provided log message.
+	 *
+	 * @param {string} format format describing message to display and log
+	 * @param {*} args data to be injected into message
+	 * @returns {void}
+	 */
+	static log( format, ...args ) {
+		process.stdout.write( Util.format( format, ...args ) + "\n" );
+
+		this.collect( format, ...args );
+	}
+
+	/**
+	 * Collects provided log message without displaying it.
+	 *
+	 * @param {string} format format describing message to log
+	 * @param {*} args data to be injected into message
+	 * @returns {void}
+	 */
+	static collect( format, ...args ) {
+		if ( server ) {
+			server.log( Buffer.from( Util.format( format, ...args ), "utf8" ), {
+				address: "127.0.0.1",
+				port: "server"
+			} );
+		} else {
+			this.transmitLog( format, ...args );
+		}
+	}
+
+	/**
+	 * Promises service hasn't received new messages for a while.
+	 *
+	 * @param {int} timeout number of tenth of a second to wait for log settling
+	 * @returns {Promise} promises service hasn't received new messages for a while
+	 */
+	settled( timeout = 50 ) {
+		if ( !( timeout > 0 ) ) {
+			return Promise.resolve();
+		}
+
+		process.stderr.write( "\nwaiting for log server to settle ...\n" );
+
+		return new Promise( resolve => {
+			let latest = this.captured;
+			let settled = 0;
+			let linear = 0;
+
+			const timer = setInterval( () => {
+				if ( ++linear >= timeout ) {
+					clearInterval( timer );
+					resolve();
+				} else if ( this.captured > latest ) {
+					settled = 0;
+				} else if ( ++settled >= 10 ) {
+					clearInterval( timer );
+					resolve();
+				}
+			}, 100 );
+		} );
+	}
+
+	/**
+	 * Dumps current set of collected log messages to stderr.
+	 *
+	 * @param {int} settleTimeout number of tenth of a second to wait for log settling before dumping
+	 * @returns {Promise} promises settled log dumped to stderr
+	 */
+	static dump( settleTimeout = 50 ) {
+		if ( !server ) {
+			process.stderr.write( `\nbacklog:\n... empty ...\n` );
+			return Promise.resolve();
+		}
+
+		return server.settled( settleTimeout ).then( () => {
+			process.stderr.write( `\nbacklog:\n` );
+
+			for ( let i = 0, numEntries = server.write; i < numEntries; i++ ) {
+				const { peer, msg } = server.logs[i];
+
+				process.stderr.write( `${peer}: ${msg}\n` );
+			}
+		} );
+	}
+
+	/**
+	 * Drops any current log server instance closing its listener socket.
+	 *
+	 * @returns {Promise} promises server dropped
+	 */
+	static drop() {
+		return new Promise( ( resolve, reject ) => {
+			if ( server ) {
+				server._socket.once( "close", resolve );
+				server._socket.once( "error", reject );
+				server._socket.close();
+
+				server = null;
+			} else {
+				resolve();
+			}
+		} );
+	}
+
+	/**
 	 * Transmits provided message to log server selected by environment
 	 * variables.
 	 *
 	 * @param {string} message log message to be transmitted
+	 * @param {*} args data to be injected into message
 	 * @returns {void}
 	 */
-	static transmitLog( message ) {
-		process.stderr.write( message + "\n" );
-
+	static transmitLog( message, ...args ) {
 		const { DEBUG_LOG_SERVER_NAME, DEBUG_LOG_SERVER_PORT } = process.env;
 
 		if ( DEBUG_LOG_SERVER_NAME && DEBUG_LOG_SERVER_PORT ) {
@@ -172,7 +309,9 @@ class LogServer {
 				client = UDP.createSocket( "udp4" );
 			}
 
-			client.send( Buffer.from( message, "utf8" ), DEBUG_LOG_SERVER_PORT, DEBUG_LOG_SERVER_NAME );
+			const msg = args.length > 0 ? Util.format( message, ...args ) : message;
+
+			client.send( Buffer.from( msg, "utf8" ), DEBUG_LOG_SERVER_PORT, DEBUG_LOG_SERVER_NAME );
 		}
 	}
 }
